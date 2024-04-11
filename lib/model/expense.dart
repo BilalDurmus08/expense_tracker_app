914fdbef21bdc9d39663f3d4053304afdb557682
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-04-11 22:31:27 +0300

    Expensebucket class added

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -4,15 +4,15 @@ import 'package:intl/intl.dart';
 
 const uuid = Uuid();
 
-final formatter = DateFormat.yMd(); 
+final formatter = DateFormat.yMd();
 
 enum Category { food, travel, leisure, work }
 
- const categoryIcons = {
-    Category.food: Icons.lunch_dining,
-    Category.leisure: Icons.movie,
-    Category.travel: Icons.flight_takeoff,
-    Category.work: Icons.work,
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.leisure: Icons.movie,
+  Category.travel: Icons.flight_takeoff,
+  Category.work: Icons.work,
 };
 
 class Expense {
@@ -29,7 +29,28 @@ class Expense {
   final DateTime dateTime;
   final Category category;
 
-  String get formattedDate { //I converted the date more readeble form.
+  String get formattedDate {
+    //I converted the date more readeble form.
     return formatter.format(dateTime);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({required this.expenses, required this.category});
+
+  ExpenseBucket.forCategory(List<Expense> allExpense, this.category)
+      : expenses = allExpense
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalBucket {
+    double total = 0;
+    for (var expense in expenses) {
+      total += expense.price;
+    }
+    return total;
   }
 }
