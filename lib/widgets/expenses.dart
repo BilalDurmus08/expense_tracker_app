8cb778bb02db8dba24947967e2808a1e1c991599
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-04-12 11:41:29 +0300

    chart classes added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/model/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -94,7 +95,7 @@ class _Expenses extends State<Expenses> {
       ]),
       body: Column(
         children: [
-          const Text('The Chart'),
+          Chart(expenses: _registeredExpenses),
           mainContent,
         ],
       ),
